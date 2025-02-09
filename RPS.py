43194d69cf1880cbeb032fc9f1c2413034dbb816
s2h25 <keithenvalentine@unomaha.edu>
2025-02-09 00:28:21 +0000

    Day3.5

--- RPS.py
+++ RPS.py
@@ -15,57 +15,57 @@ def main():
   #Randomly choose the computer between 'R', 'P', 'S'
        computer = random.choice(["R","P", "S"])
   #Prompt the user for their RPS selection
-       player = input("Pick your fighter (R, P, S)")
+       player = input("Pick your fighter (R, P, S)").upper()
   #Protect from invalid input
        if player not in ["R", "P", "S"]:
            print("Invalid fighter. Please pick R, P, or S.")
            continue
   #Determine winner and state what happened to the user
        if computer == "R":
-        print("computer chose Rock")
+          print("computer chose Rock")
        elif computer == "P":   
-        print("computer chose paper")
+          print("computer chose paper")
        else:
-        print("computer chose scissors")  
+          print("computer chose scissors")  
        if player == "R":
-        print("player chose Rock")
+          print("player chose Rock")
        elif player == "P":   
-        print("player chose paper")
+          print("player chose paper")
        else:
-        print("player chose scissors")  
+          print("player chose scissors")  
        if player == "R" and computer == "R":
-        print("Tie")
-        ties = ties + 1
+          print("Tie")
+          ties = ties + 1
        if player == "R" and computer == "P":
-        print("Computer wins.")
-        losses = losses + 1
+          print("Computer wins.")
+          losses = losses + 1
        if player == "R" and computer == "S":
-        print("You win!")
-        wins = wins + 1
+          print("You win!")
+          wins = wins + 1
        if player == "S" and computer == "S":
-        print("Tie")
-        ties = ties + 1
+          print("Tie")
+          ties = ties + 1
        if player == "S" and computer == "R":
-        print("Computer wins.")
-        losses = losses + 1
+          print("Computer wins.")
+          losses = losses + 1
        if player == "S" and computer == "P":
-        print("You win!")
-        wins = wins + 1       
+          print("You win!")
+          wins = wins + 1       
        if player == "P" and computer == "P":
-        print("Tie")
-        ties = ties + 1
+          print("Tie")
+          ties = ties + 1
        if player == "P" and computer == "S":
-        print("Computer wins.")
-        losses = losses + 1
+          print("Computer wins.")
+          losses = losses + 1
        if player == "P" and computer == "R":
-        print("You win!")
-        wins = wins + 1  
+          print("You win!")
+          wins = wins + 1  
  #Print Stats       
-        print("Wins \t Ties \t Losses")
-        print("---- \t ---- \t ------")
-        print(wins, "\t", ties, "\t", losses) 
+        
+       print("Wins \t Ties \t Losses")
+       print("---- \t ---- \t ------")
+       print(wins, "\t", ties, "\t", losses) 
 # Ask user if they want to play again
-
-        playAgain = input("Play again? (Y/N):" ).upper()   
+       playAgain = input("\nPlay again? (Y/N): ").upper()   
 if __name__ == '__main__':
   main()
